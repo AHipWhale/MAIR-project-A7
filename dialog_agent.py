f5363f5fb57e451c8de6f86742bbba0199903597
AHipWhale <charlie.choffat@gmail.com>
2025-09-20 16:27:15 +0200

    - Added code for easy debugging (default is off) - Changed response_utterance for if there are multiple or one restaurants that meet requirements

--- dialog_agent.py
+++ dialog_agent.py
@@ -5,7 +5,7 @@ from infer import infer_utterance, load_artifacts
 from keyword_extractor import extract_keywords
 
 class dialogAgent():
-    def __init__(self, model_path=None, restaurant_path="datasets/restaurant_info.csv"):
+    def __init__(self, model_path=None, restaurant_path="datasets/restaurant_info.csv", debug_mode=False):
         """
         Initialize dialog agent
         """
@@ -29,6 +29,9 @@ class dialogAgent():
         # Last suggested restaurant
         self.sugg_restaurant = None
 
+        # For debugging during development
+        self.debug_mode = debug_mode
+
     def start_dialog(self):
         # Loop state_transition until certain state is reached (end state)
         state = None
@@ -37,7 +40,8 @@ class dialogAgent():
             # State transition
             state, system_utterance = self.__state_transition(state, user_input)
 
-            print("")
+            if self.debug_mode:
+                print("")
 
             if state != "9.1 Goodbye":
                 # Ouput system_utterance
@@ -80,7 +84,10 @@ class dialogAgent():
         # Classify dialog act (could be call to function)
         if utterance != None:
             classified_dialog_act = infer_utterance(self.model, self.vectorizer, self.label_encoder, self.metadata, utterance)
-            print(f"user utterance classified as: {classified_dialog_act}")
+
+            if self.debug_mode:
+                print(f"user utterance classified as: {classified_dialog_act}")
+
         else:
             classified_dialog_act = ""
 
@@ -94,9 +101,10 @@ class dialogAgent():
             if output["food"] != None:
                 self.food = output['food']
 
-            print(f"Area changed to: {self.area}") # debug
-            print(f"Price changed to: {self.price}") # debug
-            print(f"Food changed to: {self.food}") # debug
+            if self.debug_mode:
+                print(f"Area changed to: {self.area}") # debug
+                print(f"Price changed to: {self.price}") # debug
+                print(f"Food changed to: {self.food}") # debug
         
         # State transitions
 
@@ -106,7 +114,8 @@ class dialogAgent():
             next_state = "1. Welcome"
             response_utterance = "Hello , welcome to the Cambridge restaurant system? You can ask for restaurants by area , price range or food type . How may I help you?"
 
-            print("Entered State '1. Welcome'")
+            if self.debug_mode:
+                print("Entered State '1. Welcome'")
         
         # State "1. Welcome" or "2.2 Ask Area" to "2.2 Ask Area"
         elif current_state in ["1. Welcome", "2.2 Ask Area"] and self.area == None: # 2.1 Area Known?
@@ -114,7 +123,8 @@ class dialogAgent():
             next_state = "2.2 Ask Area"
             response_utterance = "What part of town do you have in mind?"
 
-            print("Entered State '2.2 Ask Area'")
+            if self.debug_mode:
+                print("Entered State '2.2 Ask Area'")
 
         # State "1. Welcome" or "2.2 Ask Area" or "3.2 Ask price" to "3.2 Ask price"
         elif current_state in ["1. Welcome", "2.2 Ask Area", "3.2 Ask price"] and self.price == None: # 3.1 Price known?
@@ -122,7 +132,8 @@ class dialogAgent():
             next_state = "3.2 Ask price"
             response_utterance = "How pricy would you like the restaurant to be?"
 
-            print("Entered State '3.2 Ask price'")
+            if self.debug_mode:
+                print("Entered State '3.2 Ask price'")
         
         # State "1. Welcome" or "2.2 Ask Area" or "3.2 Ask price" or "4,2 Ask Food type" to "4.2 Ask Food type"
         elif current_state in ["1. Welcome", "2.2 Ask Area", "3.2 Ask price", "4.2 Ask Food type"] and self.food == None: # 4.1 Food type known?
@@ -130,7 +141,8 @@ class dialogAgent():
             next_state = "4.2 Ask Food type"
             response_utterance = "What kind of food would you like?"
 
-            print("Entered State '4.2 Ask Food type'")
+            if self.debug_mode:
+                print("Entered State '4.2 Ask Food type'")
         
         # State "1. Welcome" or "2.2 Ask Area" or "3.2 Ask price" or "4.2 Ask Food type" or "5.2 Change 1 of preferences" to "5.2 Change 1 of preferences" or "6.1 Suggest restaurant"
         elif current_state in ["1. Welcome", "2.2 Ask Area", "3.2 Ask price", "4.2 Ask Food type", "5.2 Change 1 of preferences"] and self.area != None and self.price != None and self.food != None: # 5.1 Is there a match
@@ -143,23 +155,27 @@ class dialogAgent():
                 # State "5.2 Change 1 of preferences"
                 next_state = "5.2 Change 1 of preferences"
                 response_utterance = "There are no restaurants that meet your requirements. Would you like to change the area, pricerange or foodtype? And what would you like to change it to?"
-            
-                print("Entered State '5.2 Change 1 of preferences'")
+
+                if self.debug_mode:
+                    print("Entered State '5.2 Change 1 of preferences'")
 
             else:
                 if possible_restaurant_count == 1:
                     self.sugg_restaurant = possible_restaurants[0]
+                    response_utterance = f"{self.sugg_restaurant['restaurantname']} is the only restaurant that meet your requirements. Would you like some infromation about this restaurant?"
                 else:
                     # Choose random restaurant and save the others
                     self.sugg_restaurant = random.choice(possible_restaurants)
                     self.restaurants = possible_restaurants
                     self.restaurants.remove(self.sugg_restaurant)
 
+                    response_utterance = f"{self.sugg_restaurant['restaurantname']} is a restaurant that meet your requirements. Would you like some infromation about this restaurant or an alternative restaurant?"
+
                 # State "6.1 Suggest restaurant"
                 next_state = "6.1 Suggest restaurant"
-                response_utterance = f"{self.sugg_restaurant['restaurantname']} is a restaurant that meet your requirements. Would you like some infromation about this restaurant or an alternative restaurant?"
 
-                print("Entered State '6.1 Suggest restaurant'")
+                if self.debug_mode:
+                    print("Entered State '6.1 Suggest restaurant'")
 
         # State "6.1 Suggest restaurant" to "6.1 Suggest restaurant"
         elif current_state == "6.1 Suggest restaurant" and classified_dialog_act == "reqalts":
@@ -169,7 +185,8 @@ class dialogAgent():
             next_state = "6.1 Suggest restaurant"
             response_utterance = f"{self.sugg_restaurant['restaurantname']} is another restaurant that meet your requirements. Would you like some infromation about this restaurant or a different restaurant?" 
 
-            print("Entered State '6.1 Suggest restaurant'")
+            if self.debug_mode:
+                print("Entered State '6.1 Suggest restaurant'")
 
         # State "6.1 Suggest restaurant" or "7.1 Provide information questioned" to "7.1 Provide information questioned"
         elif current_state in ["6.1 Suggest restaurant", "7.1 Provide information questioned"] and classified_dialog_act == "request":
@@ -186,7 +203,8 @@ class dialogAgent():
             # State "7.1 Provide information questioned"
             next_state = "7.1 Provide information questioned"
 
-            print("Entered State '7.1 Provide information questioned'")
+            if self.debug_mode:
+                print("Entered State '7.1 Provide information questioned'")
 
         # State "6.1 Suggest restaurant" to "8.1 Last restaurant statement"    
         elif current_state in ["6.1 Suggest restaurant", "7.1 Provide information questioned"] and (classified_dialog_act == "confirm" or classified_dialog_act == "null"):
@@ -194,7 +212,8 @@ class dialogAgent():
             next_state = "8.1 Last restaurant statement"
             response_utterance = f"Restaurant {self.sugg_restaurant['restaurantname']} is a great restaurant"
 
-            print("Entered State '8.1 Last restaurant statement'")
+            if self.debug_mode:
+                print("Entered State '8.1 Last restaurant statement'")
         
         # State "8.1 Last restaurant statement" or "7.1 Provide information questioned" to "9.1 Goodbye"
         elif current_state in ["7.1 Provide information questioned", "8.1 Last restaurant statement"] and classified_dialog_act == "bye":
@@ -202,7 +221,8 @@ class dialogAgent():
             next_state = "9.1 Goodbye"
             response_utterance = None
 
-            print("Entered State '9.1 Goodbye'")
+            if self.debug_mode:
+                print("Entered State '9.1 Goodbye'")
         else:
             next_state = current_state
             response_utterance = "I didn't understand, could you rephrase it in a different way?"
